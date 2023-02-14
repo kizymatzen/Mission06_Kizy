8802fb1f5c3d48b4119ef00a1c9b5cf85d187e72
Kizy Matzenbacher <kizymatzen@gmail.com>
2023-02-13 23:00:22 -0700

    Add project files.

---
+++ Mission06_Kizy/Models/MovieApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_Kizy.Models
+{
+    public class MovieApplicationContext : DbContext
+    {
+        //Constructor
+        public MovieApplicationContext(DbContextOptions<MovieApplicationContext> options): base (options)
+        {
+            //Leave blank for now
+        }
+        
+        public DbSet<ApplicationResponse> responses { get; set; }
+    }
+}
